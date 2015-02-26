855255b31653ac4d3f1d282fecd9a0de8a19dc74
Tomás Aparicio <tomas@aparicio.me>
2015-02-26 12:42:01 +0100

    feat: add installer script

---
+++ installer.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+# nar-installer - Simple Bash script to install packages like using `npm install -g`
+# License: wtfpl
+#
+
+# Customize this!
+VERSION=$1
+NAME="package"
+URL="https://github.com/you/project/releases/download/$VERSION/$NAME-$VERSION"
+
+os=`uname`
+if [[ "$os" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$os" == 'Darwin' ]]; then
+   platform='darwin'
+else
+  echo "Unsupported operative system. Only Linux and OSX are supported" && exit 1
+fi
+
+if [ `uname -m` != 'x86_64' ]; then
+  echo "Unsupported processor architecture. Only x64 is supported" && exit 1
+fi
+
+if [ -z `which curl` ]; then
+  echo "Cannot find curl binary in PATH. Install it to continue" && exit 1
+fi
+
+INSTALL_PATH="$HOME/.npm/$NAME"
+if [ ! -d $INSTALL_PATH ]; then
+  INSTALL_PATH="$HOME/.$NAME"
+fi
+
+if [ -d $INSTALL_PATH" ]; then
+  echo "Package $NAME is already installed in $INSTALL_PATH.\nRemove it to continue"
+  exit 1
+fi
+
+echo "Downloading package $NAME v.$VERSION"
+echo
+
+download="$URL-$platform-x64.nar"
+curl -k -L $download -o $NAME.nar
+
+if [ $? != 0 ]; then
+  echo "Error while downloading binary from $download.\nExit code $?" && exit 1
+fi
+
+bash $NAME.nar extract -o $INSTALL_PATH
+mv .nar/bin/node $INSTALL_PATH/bin
+
+echo "#!/usr/bin/env bash" > $INSTALL_PATH/bin/_run
+echo 'script="$(readlink ${BASH_SOURCE[0]})"' >> $INSTALL_PATH/bin/_run
+echo 'base="$(dirname $script)"' >> $INSTALL_PATH/bin/_run
+echo '$base/node $base/'"$NAME "'$*' >> $INSTALL_PATH/bin/_run
+
+if [ -f /usr/bin/$NAME ]; then
+  rm -f /usr/bin/$NAME
+fi
+
+ln -s $INSTALL_PATH/bin/_run /usr/bin/$NAME
+chmod +x $INSTALL_PATH/bin/$NAME
+chmod +x /usr/bin/$NAME
+
+rm -f $NAME.nar
+rm -rf .nar
+
+echo
+echo "The installation of $NAME was completed"
+echo
+echo "This package is also available from PATH. Use:"
+echo "$NAME --version"
+echo
